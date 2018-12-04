722727fa8da653db8fbfaa7854a2d06dde37cdef
Ariel Balita <ariel.a.balita@gmail.com>
2018-12-04 15:41:03 +0800

    update

---
+++ app.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var express = require("express");
+var path = require("path");
+var logger = require("morgan");
+var bodyParser = require("body-parser");
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: "false"
+  })
+);
+
+
+//Routes
+//#############################################################################
+app.get('/', (req, res)=>{
+  res.json('This is travis-ci-sample Project')
+});
+
+module.exports = app;
